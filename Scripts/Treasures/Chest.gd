0fee2bfb62cffb5eddce2c1b6afe01a17410dbbd
Yuna <ringale@vkarch.com>
2025-08-01 10:13:56 +0530

    Fixed UI

--- Scripts/Treasures/Chest.gd
+++ Scripts/Treasures/Chest.gd
@@ -2,15 +2,14 @@ extends Area2D
 
 @export var _is_open : bool
 @export var _is_locked : bool
-@export_range (0,99) var _total_value : int = 1
+@export_range(0, 99) var _total_value : int = 1
 @export var _silver_coin : PackedScene
 @export var _padlock : PackedScene
 @export var _gold_coin : PackedScene
 
 @onready var _rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
-var _booty : Array[Treasure]
-
+var _booty : Array[Treasure] = []
 
 func _ready():
 	while _total_value > 10:
@@ -24,19 +23,25 @@ func plunder():
 	for item in _booty:
 		item.global_position = global_position + Vector2.UP * Global.pixelpertile
 		item.freeze = false
-		item.apply_impluse(Vector2.UP * Global.pixelpertile * _rng.randf_range(5,10) + Vector2.RIGHT * Global.pixelpertile * _rng.randf_range(-1,1))
+		item.apply_impulse(
+			Vector2.UP * Global.pixelpertile * _rng.randf_range(5, 10) +
+			Vector2.RIGHT * Global.pixelpertile * _rng.randf_range(-1, 1)
+		)
 		get_parent().add_child(item)
 	_booty.clear()
 
 func throw_padlock():
-	var padlock : RigidBody2D = _padlock.instantiate()
+	var padlock: RigidBody2D = _padlock.instantiate()
 	padlock.global_position = global_position + Vector2(-4, 9)
-	padlock.apply_impluse(Vector2.UP * Global.pixelpertile + Vector2.RIGHT * Global.pixelpertile * _rng.randf_range(-1,1))
+	padlock.apply_impulse(
+		Vector2.UP * Global.pixelpertile +
+		Vector2.RIGHT * Global.pixelpertile * _rng.randf_range(-1, 1)
+	)
 	get_parent().add_child(padlock)
 
 func _on_body_entered(body: Node2D):
 	if body is Character:
-		if _is_locked && Unit.data.has_key:
+		if _is_locked and Unit.data.has_key:
 			_is_locked = false
 			$/root/Game.use_key()
 		if not _is_locked:
--- Scripts/Treasures/Treasure.gd
+++ Scripts/Treasures/Treasure.gd
@@ -3,12 +3,14 @@ class_name Treasure extends CollisionObject2D
 
 @onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var _sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
+var _character : Character
 
 
 func _on_body_entered(body: Node) -> void:
 	_sfx.play()
 	if not body is Character:
 		return
+	_character = body
 	_collect()
 	collision_mask = 0
 	_sprite.play("Effect")
